d0240b2cc27770d175179214e54e16514e9b0d57
Sam <anothercat1259@gmail.com>
2021-12-10 16:33:11 +1300

    feat: add customisable logging level

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ A dockerized implementation of https://github.com/message-manager-discord/redis-
 - `REDIS_PORT`: Port REJSON instance is on.
 - `REDIS_HOST`: Host REJSON instance is on.
 - `DISCORD_TOKEN`: Discord bot token.
+- `LOGGING_LEVEL`: Logging level, one of the npm levels found [here](https://github.com/winstonjs/winston#logging-levels). Defaults to `info`.
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { createGatewayConnection } from "redis-discord-cache";
+import winston from "winston";
 
 const HOST = process.env.REDIS_HOST;
 const PORT_string = process.env.REDIS_PORT;
 const TOKEN = process.env.DISCORD_TOKEN;
+
 if (!HOST || !PORT_string || !TOKEN) {
   throw new Error("Missing environment variables");
 }
@@ -13,6 +15,12 @@ try {
   throw new Error("Port must be a valid number");
 }
 
+let LOGGING_LEVEL = process.env.LOGGING_LEVEL;
+
+if (!LOGGING_LEVEL) {
+  LOGGING_LEVEL = "info";
+}
+
 createGatewayConnection({
   redis: { host: HOST, port: PORT },
   discord: {
@@ -21,4 +29,14 @@ createGatewayConnection({
       status: "online",
     },
   },
+  logger: winston.createLogger({
+    level: LOGGING_LEVEL,
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.simple(),
+        handleExceptions: true,
+      }),
+    ],
+    exitOnError: false,
+  }),
 });
